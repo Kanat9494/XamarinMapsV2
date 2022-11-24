c0494185e63515637d7974710b54f60b7de5e24a
kana2022 <kkanat94@gmail.com>
2022-11-24 12:53:16 +0600

    Currently finished it

--- XamarinMapsV2/XamarinMapsV2/MainPage.xaml.cs
+++ XamarinMapsV2/XamarinMapsV2/MainPage.xaml.cs
@@ -7,11 +7,16 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using Xamarin.Essentials;
+using Plugin.Permissions;
 
 namespace XamarinMapsV2
 {
     public partial class MainPage : ContentPage
     {
+        private bool hasLocationPermission = false;
         public MainPage()
         {
             InitializeComponent();
@@ -29,7 +34,7 @@ namespace XamarinMapsV2
                     await DisplayAlert("Разрешить местположение", "Вы должны разрешить показывать местопожоление", "Ок");
                 }
 
-                if (status != PermissionStatus.Granted)
+                if (status != Plugin.Permissions.Abstractions.PermissionStatus.Granted)
                 {
                     var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.LocationWhenInUse);
 
@@ -39,14 +44,63 @@ namespace XamarinMapsV2
                     }
                 }
 
-                if (status == PermissionStatus.Granted)
+                if (status == Plugin.Permissions.Abstractions.PermissionStatus.Granted)
+                {
+                    hasLocationPermission = true;
                     localMap.IsShowingUser = true;
+                    GetLocation();
+                }
                 else
                     await DisplayAlert("Местоположение отказано", "Невозможно показать ваше местоположение на карте",
                         "Ок");
             }
             catch (Exception ex) { }
             
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+                locator.PositionChanged += Locator_PositionChanged;
+
+                await locator.StartListeningAsync(TimeSpan.Zero, 100);
+            }
+            
+
+            GetLocation();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            CrossGeolocator.Current.StopListeningAsync();
+            CrossGeolocator.Current.PositionChanged -= Locator_PositionChanged;
+        }
+
+        private async void GetLocation()
+        {
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+                var position = await locator.GetPositionAsync();
+
+                MoveMap(position);
+            }
+        }
+
+        private void MoveMap(Position position)
+        {
+            var centerMap = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+            localMap.MoveToRegion(new Xamarin.Forms.Maps.MapSpan(centerMap, 2, 2));
+        }
+
+        private void Locator_PositionChanged(object sender, PositionEventArgs e)
+        {
+            MoveMap(e.Position);
         }
     }
 }
